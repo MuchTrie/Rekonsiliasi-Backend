5e04ce3767c32e0ff79e453c54e2b2a848ca77a6
Muhammad Naufal <muhammadnaufaall@gmail.com>
2025-12-02 14:32:57 +0700

    refactor: allow all origin

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -10,13 +10,13 @@ import (
 // CORSMiddleware returns a CORS middleware configured for development
 func CORSMiddleware() gin.HandlerFunc {
 	config := cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173", "http://localhost:5174"},
+		AllowAllOrigins:  true,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length", "Content-Disposition"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}
-	
+
 	return cors.New(config)
 }
